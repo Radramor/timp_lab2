ef5212244ac0bc2c6003043cc5b2f8eaf3514616
Radramor <Dremas2003@gmail.com>
2023-03-25 18:10:51 +0700

    Добавьте файлы проекта.

---
+++ timp_lab2/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace timp_lab2
+{
+    internal class Menu
+    {
+        //string path = "Me";
+
+         
+        /*public Menu(string FileName)
+        {
+            fileName = FileName;
+        }*/
+    }
+}
---
+++ timp_lab2/MenuWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace timp_lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        
+
+        public MenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        public List<string> names = new List<string>();
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string path = "1.txt";
+            /*using(StreamReader sr = new StreamReader(path, true)
+            {
+                string line = sr.ReadLine();
+                while (line != null)
+                {                   
+                    names.Add(line);
+                    line = sr.ReadLine();
+                }
+            }*/
+
+            MenuItem item = new MenuItem();
+            item.Header = "Опция";
+            MenuItem item2 = new MenuItem();
+            item2.Header = ";
+            MenuItem item3 = new MenuItem();
+            item3.Header = names[2];
+
+            item2.Items.Add(item3);
+            item.Items.Add(item2);
+        
+            _menu.Items.Add(item);
+            //item.Items.Add(item.Header);
+            //_menu.File;
+        }
+    }
+}
