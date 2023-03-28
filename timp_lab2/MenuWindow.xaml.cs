ba41989b269b49ba7c35d2b864f708f820f71ade
the-asind <84527186+the-asind@users.noreply.github.com>
2023-03-28 13:49:54 +0700

    Added parent stack; Added data integrity checks; Added data correctness checks; Closed all TODOs.

--- timp_lab2/MenuWindow.xaml.cs
+++ timp_lab2/MenuWindow.xaml.cs
@@ -1,82 +1,120 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace timp_lab2;
 
-public partial class MenuWindow : Window
+public partial class MenuWindow
 {
     public MenuWindow( /*string path*/)
     {
         InitializeComponent();
-        
-        List<List<object>> dataList = ParseData("Data/Data.txt");
+
+        var dataList = ParseData("Data/Data.txt");
         AddMenu(dataList);
     }
 
-    private List<List<object>> ParseData(string filePath)
+    private void AddMenu(List<List<object>> dataList)
     {
-        // Считывание данных из файла
-        var lines = File.ReadAllLines(filePath);
-
-        // Разбирает каждую строку и добавляет её в List
-        return (from line in lines
-            select Regex.Match(line, @"(\d+)\s+(.+)\s+(\d+)\s*(.*)")
-            into match
-            where match.Success
-                let num1 = int.Parse(match.Groups[1].Value)
-                let str1 = match.Groups[2].Value
-                let num2 = int.Parse(match.Groups[3].Value)
-                let str2 = match.Groups[4].Value
-            select new List<object> { num1, str1, num2, str2 }).ToList();
-    }
-
-    public void AddMenu(List<List<object>> dataList)
-    {
-        var parents = new Dictionary<int, MenuItem>();
-
-        foreach (var data in dataList)
+        var parentStack = new Stack<MenuItem>();
+    
+        for (var i = 0; i < dataList.Count; i++)
         {
-            var level = (int)data[0];
-            var name = (string)data[1];
-            var visibility = (int)data[2];
-            var handler = (string)data[3];
+            var level = (int)dataList[i][0];
+            var name = (string)dataList[i][1];
+            var visibility = (int)dataList[i][2];
+            var handler = (string)dataList[i][3];
 
             if (visibility == 2) continue; // Пропустить невидимые элементы
 
-            var item = new MenuItem();
-            item.Header = name;
+            var item = new MenuItem
+            {
+                Header = name
+            };
 
-            if (handler != "") // Если есть обработчик, добавить событие клика TODO: если там обработчик и дальше есть ниже иерархия, то кинуть ошибку
-                item.Click += (sender, e) =>
-                {
-                    MethodInfo? method = typeof(MenuWindow).GetMethod(handler); //TODO: можно попарсить ошибки в data.txt
-                    method?.Invoke(null, null); //TODO: if (method == null) {
-                };
+            if (handler != "") // Если есть обработчик, добавить событие клика 
+            {
+                var method = typeof(MenuWindow).GetMethod(handler);
+                if (method == null) 
+                    throw new DataException($"Handler {handler} not found for {name}");
+                
+                // Провертьб есть ли у элемента дети и обработчик одновременно
+                if (dataList.Count > i + 1 && (int)dataList[i + 1][0] > level)
+                    throw new DataException($"Item {name} cannot have both a handler and children");
+                
+                // Добавить событие клика
+                item.Click += (sender, e) => 
+                    { method.Invoke(null, null); };
+            }
+            else // Проверить наличие детей при отсутствии обработчика
+            {
+                if (dataList.Count == i || (int)dataList[i + 1][0] <= level)
+                    throw new DataException($"Item {name} must have either a handler or children");
+            }
 
             if (visibility == 1) // Если нужно выключить элемент
+            {
                 item.IsEnabled = false; // Выключить элемент
+            }
 
             if (level == 0) // Элемент верхнего уровня
             {
                 _menu.Items.Add(item);
-                parents[level] = item;
+                parentStack.Push(item);
             }
             else // Подпункт
             {
-                var parent = parents[level - 1];
-                parent.Items.Add(item);
-                parents[level] = item;
+                if (i != 0)
+                {
+                    var prevLevel = (int)dataList[i - 1][0];
+                    while (prevLevel > level-1)
+                    {
+                        prevLevel--;
+                        parentStack.Pop();
+                        
+                    }
+                    var parent = parentStack.Peek();
+                    parent.Items.Add(item);
+                    parentStack.Push(item);
+                }
+                else
+                {
+                    throw new DataException($"First item {name} should be first in hierarchy");
+                }
             }
         }
+    } 
+    private List<List<object>> ParseData(string filePath)
+    {
+        // Считывание данных из файла
+        var lines = File.ReadAllLines(filePath);
+        
+        // Разбирает каждую строку и добавляет её в List
+        var dataList = (from line in lines
+            select Regex.Match(line, @"(\d+)\s+(.+)\s+(\d+)\s*(.*)")
+            into match
+            where match.Success
+            let num1 = int.Parse(match.Groups[1].Value)
+            let str1 = match.Groups[2].Value
+            let num2 = int.Parse(match.Groups[3].Value)
+            let str2 = match.Groups[4].Value
+            select new List<object> { num1, str1, num2, str2 }).ToList();
+        
+        if (dataList.Count != lines.Length)
+            throw new DataException($"{dataList.Count-lines.Length} lines of data are corrupted");
+        
+        return dataList;
     }
+
+    
 
     public static void Stuff()
     {
-        MessageBox.Show("Hello world!", "Fuck");
+        MessageBox.Show("Hello world!", "Some caption");
     }
 }
